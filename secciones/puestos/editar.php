6143863c51a9043a0f69bc3cf67641c74ad7a4f6
Bustagar <sebastian.bustamantegarcia@gmailcom>
2023-06-11 01:46:52 -0300

    Creacion del boton editar puesto

--- secciones/puestos/editar.php
+++ secciones/puestos/editar.php
@@ -1,4 +1,46 @@
+<?php 
+require_once("../../BD.php");
+if (isset($_GET["txtID"])) {
+    $txtID = (isset($_GET["txtID"]) ? $_GET["txtID"] : "");
+    $sentencia = $conexion->prepare("SELECT * FROM `tbl_puestos` WHERE ID=:ID");
+    $sentencia->bindValue(":ID", $txtID);
+    $sentencia->execute();
+    $registro = $sentencia->fetch(PDO::FETCH_LAZY);
+    $puesto = $registro['puesto'];
+}
+if($_POST){
+    $txtID = (isset($_POST["txtID"]) ? $_POST["txtID"] : "");
+    $nombredelpuesto=(isset($_POST["nombredelpuesto"])? $_POST["nombredelpuesto"] : "");
+    $sentencia = $conexion->prepare("UPDATE `tbl_puestos` SET `puesto`=:puesto WHERE ID=:ID");
+    $sentencia->bindValue(":nombredelpuesto",$nombredelpuesto);
+    $sentencia->bindValue(":ID", $txtID);
+    $sentencia->execute();
+    header("Location:index.php");
+}
+?>
+
+
 <?php require_once("../../templates/header.php") ?>
-Edición de Puestos
+
+<div class="card">
+    <div class="card-header">
+        Editar Puestos
+    </div>
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="txtID" class="form-label">ID:</label>
+                <input type="text"
+                class="form-control" name="txtID" id="txtID" aria-describedby="helpId" value="<?php echo $txtID;?>" placeholder="" readonly> <!-- en vez de poner read only, se puede poner disable -->
+            
+                <label for="nombredelpuesto" class="form-label">Nombre del Puesto</label>
+                <input type="text"
+                class="form-control" name="nombredelpuesto" id="nombredelpuesto" aria-describedby="helpId" value="<?php echo $puesto;?>" placeholder="">
+            </div>
+            <button type="submit" name="" id="" class="btn btn-primary" role="button">Editar Registro</button>
+            <a name="" id="" class="btn btn-danger" href="./index.php" role="button">Cancelar</a>
+        </form>
+    </div>
+</div>
 
 <?php require_once("../../templates/footer.php") ?>
